d30f0bb1a68c71d715c36a782360c09334b8c3b7
Dmitriy Kazakov <hubertfu@student.21-school.ru>
2022-07-09 15:42:37 +0300

    binary functions: add s21_set_sign

--- src/s21_decimal/binary/base.c
+++ src/s21_decimal/binary/base.c
@@ -1,3 +1,4 @@
+#include "./../types.h"
 
 /**
  * @brief 
@@ -45,4 +46,24 @@ int s21_inverseBit(int number, int index) {
  */
 int s21_reset_bit(int number, int index) {
     return number &~(1 << index);
+}
+
+/**
+ * @brief устанавливает для decimal знак sign
+ * 
+ * @author Hubert Furr (hubertfu@student.21-school.ru)
+ * @param decimal меняемый decimal
+ * @param sign устанавливаемый знак, 1 - отрицательный decimal, 0 - положительный decimal
+ * любой sign, отличный от 0, установит знак 1
+ */
+void s21_set_sign(s21_decimal *decimal, int sign) {
+    decimal_bit3 bits3;
+    bits3.i = decimal->bits[3];
+    if (sign == 0) {
+        bits3.parts.sign = 0;
+    } else {
+        bits3.parts.sign = 1;
+    }
+
+    decimal->bits[3] = bits3.i;
 }
