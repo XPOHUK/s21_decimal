95d5e52900347d3d4136cc9873c5fdf05da437cb
Dmitriy Kazakov <hubertfu@student.21-school.ru>
2022-07-29 11:53:38 +0300

    conversion & truncate: final versions

--- src/s21_decimal/conversion/from_decimal_to_float.c
+++ src/s21_decimal/conversion/from_decimal_to_float.c
@@ -1,27 +1,40 @@
-#include <stdio.h>
 #include <math.h>
 
-#include "./conversion.h"
-#include "./../helpers/helpers.h"
 #include "./../binary/binary.h"
+#include "./../comparison/comparison.h"
+#include "./../helpers/helpers.h"
+#include "./conversion.h"
 
 /**
- * @brief брифы никому не нужны
- * 
+ * @brief конвертирует decimal src в float dst
+ *
+ * 078ee41e268ebc6fdbced3ebb41c40559be01974 - non-full-working version with alternative algorithm
+ *
  * @author Hubert Furr (hubertfu@student.21-school.ru)
- * @param src 
- * @param dst 
- * @return int 
+ * @param src конвертируемый decimal
+ * @param dst указатель на float, в который будет записан результат конвертации
+ * @return int код ошибки:
+ * 0 - OK
+ * 1 - ошибка конвертации
  */
-
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
     s21_conversion_result code = S21_CONVERSION_OK;
-    if (!dst || !s21_is_correct_decimal(src)) {
+    if (!dst) {
         code = S21_CONVERSION_ERROR;
+    } else if (!s21_is_correct_decimal(src)) {
+        code = S21_CONVERSION_ERROR;
+        *dst = 0.0;
+    } else if (s21_is_equal(src, s21_decimal_get_zero())) {
+        int sign = s21_decimal_get_sign(src);
+        if (sign == S21_NEGATIVE) {
+            *dst = -0.0;
+        } else {
+            *dst = 0.0;
+        }
+        code = S21_CONVERSION_OK;
     } else {
-        // TODO(hubertfu): переделать на математику decimal и алгоритм из IEEE 754
-        // включая округление
-        float tmp = 0.0;
+        *dst = 0.0;
+        double tmp = 0.0;
 
         int sign = s21_decimal_get_sign(src);
         int power = s21_decimal_get_power(src);
@@ -31,19 +44,13 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
                 tmp += pow(2.0, i);
             }
         }
-        // printf("{%f}\n", tmp);
 
-        while (power > 0) {
-            tmp /=  10;
-            --power;
-        }
-        // printf("{%f}\n", tmp);
+        double powerten = pow(10, power);
+        tmp /= powerten;
 
-        if (sign) {
-            // и пофиг на -0.0
+        if (sign == S21_NEGATIVE) {
             tmp *= -1.0;
         }
-        printf("ld:%f\n", tmp);
 
         *dst = (float)tmp;
     }
