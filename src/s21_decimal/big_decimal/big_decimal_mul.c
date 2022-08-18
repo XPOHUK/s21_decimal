e58245b50ff6eb5a1384d3ffa84210deb02ae8ac
Dmitriy Gubankov <dmitriy@gubankov.ru>
2022-08-18 11:11:23 +0300

    Begin implementing multiplication

---
+++ src/s21_decimal/big_decimal/big_decimal_mul.c
@@ -0,0 +1,16 @@
+/**
+ * @file big_decimal_mul.c
+ * @brief Умножение больших чисел
+ * @author Dmitriy Gubankov
+ * @version 0.1
+ * @date 2022-08-18
+ */
+
+#include "big_decimal.h"
+#include "../arithmetic/arithmetic.h"
+
+int big_decimal_mul(big_decimal first, big_decimal second, big_decimal* result) {
+    s21_arithmetic_result code = S21_ARITHMETIC_OK;
+    *result = big_decimal_mul_big_int(first, second);
+}
+
