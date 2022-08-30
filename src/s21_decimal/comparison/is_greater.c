b17d5f27124bbcd0df393a3f03e76dabdb8863e2
isleanna <isleanna@student.21-school.ru>
2022-08-30 21:33:27 +0300

    all done except s21_round

--- src/s21_decimal/comparison/is_greater.c
+++ src/s21_decimal/comparison/is_greater.c
@@ -1,5 +1,6 @@
 #include "./comparison.h"
-#include "../binary/binary.h"
+#include "./../binary/binary.h"
+#include "./../big_decimal/big_decimal.h"
 #include "./../helpers/helpers.h"
 /**
  * @file is_greater.c
@@ -15,37 +16,39 @@
  */
 int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
     s21_comparison_result code = S21_COMPARISON_FALSE;
+    big_decimal big_decimal_1 = decimal_to_big_decimal(value_1);
+    big_decimal big_decimal_2 = decimal_to_big_decimal(value_2);
 
     // Извлекаем знаки обоих значений и записываем их в соответствующие переменные.
-    int value_1_sign = s21_decimal_get_sign(value_1);
-    int value_2_sign = s21_decimal_get_sign(value_2);
+    int big_decimal_1_sign = big_decimal_get_sign(big_decimal_1);
+    int big_decimal_2_sign = big_decimal_get_sign(big_decimal_2);
 
     // Если оба значения равны.
     if (s21_is_equal(value_1, value_2)) {
         code = S21_COMPARISON_FALSE;
     // Если знак первого значения отрицательный, а второго - положительный.
-    } else if (value_1_sign > value_2_sign) {
+    } else if (big_decimal_1_sign > big_decimal_2_sign) {
         code = S21_COMPARISON_FALSE;
     // Если знак первого значения положительный, а второго - отрицательный.
-    } else if (value_1_sign < value_2_sign) {
+    } else if (big_decimal_1_sign < big_decimal_2_sign) {
         code = S21_COMPARISON_TRUE;
     // Если знаки обоих значений равны, то выполняется побитовое сравнение.
-    } else {  // if (value_1_sign == value_2_sign) {
-        // Здесь необходима функция для выравнивания степеней(&value_1, &value_2);
-        for (int i = 95; i >= 0; i--) {
-            int value_1_bit = s21_decimal_is_set_bit(value_1, i);
-            int value_2_bit = s21_decimal_is_set_bit(value_2, i);
+    } else if (big_decimal_1_sign == big_decimal_2_sign) {
+        big_decimal_balance_exp(&big_decimal_1, &big_decimal_2);
+        for (int i = 191; i >= 0; i--) {
+            int big_decimal_1_bit = big_decimal_is_set_bit(big_decimal_1, i);
+            int big_decimal_2_bit = big_decimal_is_set_bit(big_decimal_2, i);
 
-            if (value_1_bit > value_2_bit) {
+            if (big_decimal_1_bit > big_decimal_2_bit) {
                 code = S21_COMPARISON_TRUE;
                 break;
-            } else if (value_1_bit < value_2_bit) {
+            } else if (big_decimal_1_bit < big_decimal_2_bit) {
                 code = S21_COMPARISON_FALSE;
                 break;
             }
         }
         // Обработка ситуации, когда знаки обоих значений отрицательные.
-        if (value_1_sign == 1) {
+        if (big_decimal_1_sign == 1) {
             if (code == S21_COMPARISON_TRUE) {
                 code = S21_COMPARISON_FALSE;
             } else {
