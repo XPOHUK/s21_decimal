5ac865ddc1920356bba3a54fce504247f17e8af1
Казаков Дмитрий <hubertfu@student.21-school.ru>
2022-08-31 14:33:28 +0300

    fix for cppcheck

--- src/tests/_helpers/_debug.h
+++ src/tests/_helpers/_debug.h
@@ -10,6 +10,19 @@
 #define OKCYAN  "\033[96m"
 #define RESET   "\033[0m"
 
+typedef union test_decimal_bytes {
+    struct s21_decimal d;
+    struct {
+        uint32_t part0;
+        uint32_t part1;
+        uint32_t part2;
+        uint32_t empty2 : 16;
+        uint32_t power : 8;
+        uint32_t empty1 : 7;
+        uint32_t sign : 1;
+    } parts;
+} test_decimal_bytes;
+
 void s21_print_bits(size_t const size, void const * const ptr, int color);
 void s21_print_bit(int number, int color);
 void s21_decimal_to_string(s21_decimal decimal, char *res);
--- src/tests/_helpers/_get_sign.c
+++ src/tests/_helpers/_get_sign.c
@@ -2,20 +2,7 @@
 #include "./_debug.h"
 
 int test_decimal_get_sign(s21_decimal value) {
-    union {
-        struct s21_decimal d;
-        struct {
-            uint32_t part0;
-            uint32_t part1;
-            uint32_t part2;
-            uint32_t empty2 : 16;
-            uint32_t power : 8;
-            uint32_t empty1 : 7;
-            uint32_t sign : 1;
-        } parts;
-    } decimal_bytes;
-
+    test_decimal_bytes decimal_bytes;
     decimal_bytes.d = value;
-
     return decimal_bytes.parts.sign;
 }
